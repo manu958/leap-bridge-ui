6c2d0539f257b1f788ba75f871d93a84d4dbe31d
manu958 <sharmamanu4546@gmail.com>
2025-10-08 16:14:09 +0530

    fixedproblemsolve code issues

--- app/components/homepage/ProblemSolv.tsx
+++ app/components/homepage/ProblemSolv.tsx
@@ -74,7 +74,7 @@ const ProblemSolv = () => {
             <p
               className={`${satoshi.className} text-[16px] md:text-[18px] lg:text-[20px] mt-2`}
             >
-              Our experts implement new processes and technology with hands-on training for your team's success.
+              Our experts implement new processes and technology with hands-on training for your team&apos;s success.
             </p>
           </div>
 
@@ -96,50 +96,6 @@ const ProblemSolv = () => {
             </p>
           </div>
         </div>
-
-        {/* <div className="md:w-1/2 w-full p-8 space-y-6 right-sticky-process text-[#1B3233]">
-
-                    <div className="bg-[#d6ebe2] rounded-lg p-28  ">
-                        <h3 className={`${zodiak.className} text-5xl font-light opacity-30 text-[100px] mb-16`}>1</h3>
-                        <h4 className={`${zodiak.className} font-normal leading-[120%] text-[26px] mb-8`}>Research</h4>
-                        <p className={`${satoshi.className} text-sm text-[20px] mt-2`}>
-                            Phasellus nec sem in justo pellentesque facilisis. Aliquam lorem
-                            ante, dapibus in, viverra quis, feugiat a, tellus. Nam pretium
-                            turpis et arcu.
-                        </p>
-                    </div>
-
-                    <div className="bg-[#d6ebe2] rounded-lg p-28">
-                        <h3 className={`${zodiak.className} text-5xl text-[100px] opacity-30 font-light mb-16`}>2</h3>
-                        <h4 className={`${zodiak.className} font-normal leading-[120%] text-[26px] mb-8`}>Ideation</h4>
-                        <p className={`${satoshi.className} text-sm text-[20px] mt-2`}>
-                            Phasellus nec sem in justo pellentesque facilisis. Aliquam lorem
-                            ante, dapibus in, viverra quis, feugiat a, tellus. Nam pretium
-                            turpis et arcu.
-                        </p>
-                    </div>
-
-                    <div className="bg-[#d6ebe2] rounded-lg p-28">
-                        <h3 className={`${zodiak.className} text-5xl font-light opacity-30 text-[100px] mb-16`}>3</h3>
-                        <h4 className={`${zodiak.className} font-normal leading-[120%] text-[26px] mb-8`}>Design</h4>
-                        <p className={`${satoshi.className} text-sm text-[20px] mt-2`}>
-                            Phasellus nec sem in justo pellentesque facilisis. Aliquam lorem
-                            ante, dapibus in, viverra quis, feugiat a, tellus. Nam pretium
-                            turpis et arcu.
-                        </p>
-                    </div>
-
-                    <div className="bg-[#d6ebe2] rounded-lg p-28">
-                        <h3 className={`${zodiak.className} text-5xl text-[100px] opacity-30 font-light mb-16`}>4</h3>
-                        <h4 className={`${zodiak.className} font-normal leading-[120%] text-[26px] mb-8`}>Develop & Test</h4>
-                        <p className={`${satoshi.className} text-sm text-[20px] mt-2`}>
-                            Phasellus nec sem in justo pellentesque facilisis. Aliquam lorem
-                            ante, dapibus in, viverra quis, feugiat a, tellus. Nam pretium
-                            turpis et arcu.
-                        </p>
-                    </div>
-
-                </div> */}
       </div>
     </section>
   );
