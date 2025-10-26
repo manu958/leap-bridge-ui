6633250c5501ec0a10787a6a84d78188970a498a
manu958 <sharmamanu4546@gmail.com>
2025-10-26 18:05:38 +0530

    chnaged logos

--- app/components/homepage/ClientList1.tsx
+++ app/components/homepage/ClientList1.tsx
@@ -6,67 +6,100 @@ export default function ClientList1() {
     <section className="w-full pt-[60px] md:pt-[60px] pb-0 bg-[#daebe3]">
       <div className="max-w-[1080px] mx-auto px-4 space-y-10 text-center">
         {/* Heading */}
-        <h2
-          className={`text-gray-800 text-center text-[22px] sm:text-[26px] md:text-[50px] font-normal mb-6 sm:mb-8 md:mb-10 leading-snug`}
-        >
+        <h2 className="text-gray-800 text-center text-[22px] sm:text-[26px] md:text-[50px] font-normal mb-6 sm:mb-8 md:mb-10 leading-snug">
           <strong>Combined</strong> Work Experience
         </h2>
 
         {/* Row 1 */}
-        <div
-          className="
-            flex flex-wrap
-            justify-center sm:justify-center md:justify-between
-            gap-4 sm:gap-6 md:gap-8
-            w-full
-          "
-        >
-          {[
-            "abbot.png",
-            "Cipla_logo.png",
-            "DANONE_LOGO_HORIZONTAL.png",
-            "emami.png",
-            "godrej.png",
-            "hershey_company-logo_brandlogos.net_pcliv.png",
-          ].map((logo, index) => (
-            <Image
-              key={index}
-              src={`/assests/header/Website-logos/${logo}`}
-              alt={`${logo.split(".")[0]} Logo`}
-              width={120}
-              height={60}
-              className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain"
-            />
-          ))}
+        <div className="flex flex-wrap justify-center sm:justify-center md:justify-between gap-4 sm:gap-6 md:gap-8 w-full">
+          <Image
+            src="/assests/header/Website-logos/abbot.png"
+            alt="Abbot Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[110px] h-auto object-contain abbot-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/Cipla_logo.png"
+            alt="Cipla Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain cipla-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/DANONE_LOGO_HORIZONTAL.png"
+            alt="Danone Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain danone-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/emami.png"
+            alt="Emami Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain emami-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/godrej.png"
+            alt="Godrej Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain godrej-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/hershey_company-logo_brandlogos.net_pcliv.png"
+            alt="Hershey Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[100px] h-auto object-contain hershey-logo"
+          />
         </div>
 
         {/* Row 2 */}
-        <div
-          className="
-            flex flex-wrap
-            justify-center sm:justify-center md:justify-between
-            gap-4 sm:gap-6 md:gap-8
-            w-full
-          "
-        >
-          {[
-            "Marico_Logo.png",
-            "mondelez-international-logo.png",
-            "nobel-hygine.png",
-            "P&G-Logo.png",
-            "parle-Agro.png",
-            "procter-and-gamble-pg-new9499.jpg",
-            "Reckitt-Benckiser-logo.png",
-          ].map((logo, index) => (
-            <Image
-              key={index}
-              src={`/assests/header/Website-logos/${logo}`}
-              alt={`${logo.split(".")[0]} Logo`}
-              width={120}
-              height={60}
-              className="w-[70px] sm:w-[90px] md:w-[80px] h-auto object-contain"
-            />
-          ))}
+        <div className="flex flex-wrap justify-center sm:justify-center md:justify-between gap-4 sm:gap-6 md:gap-8 w-full">
+          <Image
+            src="/assests/header/Website-logos/Marico_Logo.png"
+            alt="Marico Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[80px] h-auto object-contain marico-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/mondelez-international-logo.png"
+            alt="Mondelez Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[110px] h-auto object-contain mondelez-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/nobel-hygine.png"
+            alt="Nobel Hygine Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[80px] h-auto object-contain nobel-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/P&G-Logo.png"
+            alt="P&G Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[80px] h-auto object-contain pg-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/parle-Agro.png"
+            alt="Parle Agro Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[80px] h-auto object-contain parle-logo"
+          />
+          <Image
+            src="/assests/header/Website-logos/Reckitt-Benckiser-logo.png"
+            alt="Reckitt Benckiser Logo"
+            width={120}
+            height={60}
+            className="w-[70px] sm:w-[90px] md:w-[110px] h-auto object-contain reckitt-logo"
+          />
         </div>
       </div>
     </section>
